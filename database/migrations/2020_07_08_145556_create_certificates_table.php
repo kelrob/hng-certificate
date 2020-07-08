a2ed741892c10d7e09184086579d6136b9b02028
kelrob <test>
2020-07-08 16:05:46 +0100

    Migration Files Added

---
+++ database/migrations/2020_07_08_145556_create_certificates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('owner', 200);
+            $table->string('email', 200);
+            $table->string('track', 100);
+            $table->integer('certificate_style');
+            $table->integer('download_count');
+            $table->string('unique_code', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}
