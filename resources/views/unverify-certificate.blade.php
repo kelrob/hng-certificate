2254516f2c23016aee8ef1296f72fbc066700091
Elisha Ukpong <Ishukpong418@gmail.com>
2020-07-09 15:54:41 +0100

    add

---
+++ resources/views/unverify-certificate.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "shortcut icon" href= "{{ url('/images/Favicon.svg') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('/css/verify.css') }}">
+    <title>HNG | CERTIFICATE DETAILS</title>
+</head>
+<body>
+<section>
+    <div>
+        <h2>Certificate Not Found</h2>
+        <h3>Status: <span id="verified" class="verified"> Not Verified</span></h3>
+    </div>
+</section>
+</body>
+</html>
